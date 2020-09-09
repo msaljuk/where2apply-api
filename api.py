01b3df6b6717862b0ca30f7a4ec007e6254f638b
msaljuk <msaljuk@stanford.edu>
2020-09-09 08:08:25 +0500

    Initial Script Commit

--- api.py
+++ api.py
@@ -1,28 +1,11 @@
-"""
-RESTful api routes for where2apply
-"""
+from flask import Flask, request, jsonify
 
-from flask import Flask
-from flask_restful import Resource, Api
+from colleges_script import get_colleges
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class Quotes(Resource):
-    def get(self):
-        return {
-            "William Shakespeare": {
-                "quote": [
-                    "Love all,trust a few,do wrong to none",
-                    "Some are born great, some achieve greatness, and some greatness thrust upon them.",
-                ]
-            },
-            "Linus": {"quote": ["Talk is cheap. Show me the code."]},
-        }
-
-
-api.add_resource(Quotes, "/")
-
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route("/api/v1/colleges/list", methods=['POST'])
+def run_colleges_script():
+    data = request.json
+    return get_colleges(data)
